4c858bfe334d2132af5db666f61e5e5403a78c10
Takuroh Shimizu <his7716twd@gmail.com>
2020-05-13 00:16:02 +0900

    完成版！

--- Count2nd/ViewController.swift
+++ Count2nd/ViewController.swift
@@ -17,6 +17,47 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    @IBAction func plus(){
+        number = number + 1
+        label.text = String(number)
+        if number >= 10 {
+            label.textColor = UIColor.red
+        }
+        else if number <= -10 {
+            label.textColor = UIColor.blue
+        }
+        else {
+            label.textColor = UIColor.black
+        }
+    }
+    
+    @IBAction func minus(){
+           number = number - 1
+           label.text = String(number)
+        if number >= 10 {
+            label.textColor = UIColor.red
+        }
+        else if number <= -10 {
+            label.textColor = UIColor.blue
+        }
+        else {
+            label.textColor = UIColor.black
+        }
+    }
+    @IBAction func clear(){
+           number = 0
+           label.text = String(number)
+        if number >= 10 {
+            label.textColor = UIColor.red
+        }
+        else if number <= -10 {
+            label.textColor = UIColor.blue
+        }
+        else {
+            label.textColor = UIColor.black
+        }
+    }
+    
 
 
 }
