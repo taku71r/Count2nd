e10588afd88620c90b236223b20b2f2d8d628720
Takuroh Shimizu <his7716twd@gmail.com>
2020-05-12 23:39:29 +0900

    ラベルの関連づけなど

--- Count2nd/ViewController.swift
+++ Count2nd/ViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var number: Int = 0
+    @IBOutlet var label: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
